59f741883193628f8347310a924ba426d3f3783d
madusha2357 <madusharajapaksha69@gmail.com>
2024-03-12 23:27:46 +0530

    added serets

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -6,6 +6,11 @@ export const environment = {
   MINIO_ACCESS_KEY: '',
   MINIO_SECRET_KEY: '',
   MINIO_BUCKET_NAME: 'damen',
+  // twilio
+  TWILIO_SID: 'AC40a443fcdf96d2be5604690c5621edfa',
+  TWILIO_TOKEN: '5264f8409433e2afe2120773afe85ab0',
+  TWILIO_VERIFY_SID: 'VA6c12d5ffacd4a51bb3892f9558db89d3',
+
   // Mongo
   MONGODB_URI:
     'mongodb://super_kolla:G2DJXVQtHp3v2yXlOQ@localhost:27017/burn?authSource=admin&readPreference=primary&ssl=false',
