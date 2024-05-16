b605e8283f2941687c9733e3dc2369a97eb22c62
madusha2357 <madusharajapaksha69@gmail.com>
2024-05-16 15:23:03 +0530

    ddq

--- apps/quize/src/app/site/site-dialog.ts
+++ apps/quize/src/app/site/site-dialog.ts
@@ -7,6 +7,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
     <div style="padding: 10px">
       <img style="width: 100%" [src]="data.imageUrl" alt="Notification Image" />
       <h2>Patient name: {{ data.name }}</h2>
+      <p>Burn level: {{ data.level }}</p>
       <p>Age: {{ data.age }}</p>
       <button class="button" style="padding: 10px" (click)="downloadDoc()">
         Download
@@ -27,9 +28,11 @@ export class NotificationDialogComponent {
   downloadDoc() {
     const link = document.createElement('a');
     const content = `
-      <img src="${this.data.imageUrl}" alt="Patient Image" style="max-width: 100%;" />
+      <h1>Burn details</h1>
+      <p>Burn level: ${this.data.level}</p>
       <p>Patient name: ${this.data.name}</p>
       <p>Age: ${this.data.age}</p>
+      <img src="${this.data.imageUrl}" alt="Patient Image" style="max-width: 100%;" />
     `;
     const blob = new Blob([content], { type: 'text/html' });
     link.setAttribute('href', URL.createObjectURL(blob));
